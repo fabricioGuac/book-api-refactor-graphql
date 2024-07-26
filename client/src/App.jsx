61f556298db25beecb3fcacde104e36ad232edec
Fabricio Guacuto <guacutofabricio@gmail.com>
2024-07-26 15:20:34 -0700

    implements the apollo provider in the App.jsx file  and refactors the saveBook function in the searchBooks.jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,49 @@
 import './App.css';
+
+import {
+  ApolloClient,
+  // InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
 import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+
+// Middleware to attach the token to every request header
+const authLink = setContext((_, {headers}) => {
+
+  const token = localStorage.getItem('id_token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "", 
+    }
+  }
+})
+
+// Set up the client to execute the authLink middleware
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  // cache: new InMemoryCache,
+})
+
+
+
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -1,4 +1,8 @@
-import { useState, useEffect } from 'react';
+import { useState, } from 'react';
+// useEffect
+
+import { useMutation } from '@apollo/client';
+
 import {
   Container,
   Col,
@@ -9,14 +13,22 @@ import {
 } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
-import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+      // saveBook,
+import { searchGoogleBooks } from '../utils/API';
+// import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+
+import {SAVE_BOOK} from '../utils/mutations';
+
+
 
 const SearchBooks = () => {
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
   // create state for holding our search field data
   const [searchInput, setSearchInput] = useState('');
+
+  const [saveBook, {error}] = useMutation(SAVE_BOOK); 
+
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
@@ -59,24 +71,16 @@ const SearchBooks = () => {
     }
   };
 
+
   // create function to handle saving a book to our database
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
-    // get token
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-    if (!token) {
-      return false;
-    }
-
     try {
-      const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      const response = await saveBook({
+        variables: {book: bookToSave}
+    });
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
